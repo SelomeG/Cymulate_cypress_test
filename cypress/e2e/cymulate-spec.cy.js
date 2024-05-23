b2d2bd90cabe7765f4dbb127aa5c6fae54e8dfcc
selome <selome>
2024-05-23 15:14:47 +0300

    all at once cymulate

---
+++ cypress/e2e/cymulate-spec.cy.js
@@ -0,0 +1,42 @@
+describe("The landing page", () => {
+  it("loads", () => {
+    cy.visit("/");
+  });
+});
+
+describe("The Login Page", () => {
+  it("It should login", function () {
+    const email = "candidate_user@cymulate1.com";
+    const password = "ZzAa!@#$4321";
+
+    cy.get("input[id=email]", { timeout: 10000 }).type(email);
+
+    cy.get("input[name=password]").type(`${password}{enter}`);
+
+    cy.url().should("include", "/cym");
+  });
+});
+
+describe("Go to Activity log and log three attack ID", () => {
+  it("Should apply filters", () => {
+    visit("/cym/activity_logs");
+    cy.get("icon_").click();
+    cy.contains("span", "Type").click();
+    cy.xpath("//button[.='Advanced Scenarios']").click();
+    cy.xpath("//button[.='Apply Filters']");
+
+    // Query all elements containing attack IDs
+    const attackIdElements = document.querySelectorAll(
+      '.MuiChip-root[test-data-id="assessmentID"] .MuiChip-label .Text__StyledSpan-sc-a1dltl-1.ljpQOx'
+    );
+
+    // Extract the attack IDs
+    const attackIds = [];
+    for (let i = 0; i < 3 && i < attackIdElements.length; i++) {
+      attackIds.push(attackIdElements[i].textContent);
+    }
+
+    // Log the first 3 attack IDs
+    cy.log("First 3 Attack IDs:", attackIds);
+  });
+});
